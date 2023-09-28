e45bf16e07f6ff74fa58436307a6ffde02fca5df
Vika <viktorykoroliova@gmail.com>
2023-09-28 15:13:12 +0300

    added custom permission IsAdminOrIfAuthenticatedReadOnly

---
+++ airport/permissions.py
@@ -0,0 +1,15 @@
+from django.views import View
+from rest_framework.permissions import SAFE_METHODS, BasePermission
+from rest_framework.request import Request
+
+
+class IsAdminOrIfAuthenticatedReadOnly(BasePermission):
+    def has_permission(self, request: Request, view: View) -> bool:
+        return bool(
+            (
+                request.method in SAFE_METHODS
+                and request.user
+                and request.user.is_authenticated
+            )
+            or (request.user and request.user.is_staff)
+        )
--- core/settings.py
+++ core/settings.py
@@ -137,6 +137,9 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
+    "DEFAULT_PERMISSION_CLASSES": [
+        "airport.permissions.IsAdminOrIfAuthenticatedReadOnly",
+    ],
 }
 
 SIMPLE_JWT = {
