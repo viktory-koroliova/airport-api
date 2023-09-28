306094f20ed2bb8ffc9d35ef5c95992c0a0b57da
Vika <viktorykoroliova@gmail.com>
2023-09-28 14:55:40 +0300

    created User model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,62 @@
-from django.db import models
+from __future__ import annotations
 
-# Create your models here.
+from typing import Any, Optional
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.base_user import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(
+            self,
+            email: str,
+            password: str,
+            **extra_fields: Any
+    ) -> User:
+        if not email:
+            raise ValueError("Users require an email field")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(
+            self,
+            email: str,
+            password: Optional[str] = None,
+            **extra_fields: Any
+    ) -> User:
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(
+            self,
+            email: str,
+            password: str,
+            **extra_fields: Any
+    ) -> User:
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(_("email address"), unique=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
