5c2578753481e87f97965d68aff000d85d336748
Vika <viktorykoroliova@gmail.com>
2023-09-28 15:17:34 +0300

    created Payment model and registered in admin panel

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from airport.models import Order
+
+
+class Payment(models.Model):
+    class Status(models.TextChoices):
+        PAID = "paid"
+        NOT_PAID = "not paid"
+
+    status = models.CharField(max_length=10, choices=Status.choices)
+    order = models.ForeignKey(Order, on_delete=models.DO_NOTHING)
+    session_url = models.CharField(max_length=255)
+    session_id = models.CharField(max_length=255)
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
