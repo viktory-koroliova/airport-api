5f39e5d72f13d964266e5664ddf9e8665a4a70c1
Vika <viktorykoroliova@gmail.com>
2023-09-28 14:50:05 +0300

    registered models in admin panel

--- airport/admin.py
+++ airport/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
-# Register your models here.
+from airport.models import (
+    AircraftManufacturer,
+    AircraftType,
+    Aircraft,
+    Airline,
+    Airport,
+    Crew,
+    Flight,
+    Route,
+    Ticket,
+    Order,
+)
+
+admin.site.register(AircraftManufacturer)
+admin.site.register(AircraftType)
+admin.site.register(Aircraft)
+admin.site.register(Airline)
+admin.site.register(Airport)
+admin.site.register(Crew)
+admin.site.register(Flight)
+admin.site.register(Route)
+admin.site.register(Ticket)
+
+
+class TicketInline(admin.TabularInline):
+    model = Ticket
+    extra = 1
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (TicketInline,)
