86ccf6e64139595c3ff61ba290038f24e341bd40
Vika <viktorykoroliova@gmail.com>
2023-09-28 15:23:10 +0300

    created Payment urls

---
+++ payments/urls.py
@@ -0,0 +1,10 @@
+from rest_framework import routers
+from payments.views import PaymentViewSet
+
+
+router = routers.DefaultRouter()
+router.register("", PaymentViewSet)
+
+urlpatterns = router.urls
+
+app_name = "payment"
