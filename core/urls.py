a2e8e76db1cd230f959ac69b749212b30ac38529
Vika <viktorykoroliova@gmail.com>
2023-09-28 14:56:48 +0300

    added user urls to global urls

--- core/urls.py
+++ core/urls.py
@@ -22,5 +22,7 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("airport.urls", namespace="airport")),
+    path("user/", include("user.urls", namespace="user")),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
